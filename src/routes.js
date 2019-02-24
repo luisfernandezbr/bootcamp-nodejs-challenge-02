bd6c2c0037916a83368ee11d981af8a38e52bbe0
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2019-02-24 20:47:24 -0300

    Using middleware to handle authorization

--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,19 @@ const multerConfig = require('./config/multer')
 const upload = require('multer')(multerConfig)
 
 const routes = express.Router()
+
+const authMiddleware = require('./app/middlewares/auth')
+const guestMiddleware = require('./app/middlewares/guest')
+
 const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
 
-routes.get('/', SessionController.create)
+routes.use('/app', authMiddleware)
+
+routes.get('/', guestMiddleware, SessionController.create)
 routes.post('/signin', SessionController.store)
 
-routes.get('/signup', UserController.create)
+routes.get('/signup', guestMiddleware, UserController.create)
 routes.post('/signup', upload.single('avatar'), UserController.store)
 
 routes.get('/app/dashboard', (req, res) => {
