ae14cc21b8b902e02508798c0c8014d159c34e6c
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2019-02-24 19:46:32 -0300

    Signup working saving encrypted password

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,8 @@
 const express = require('express')
-
 const routes = express.Router()
+const UserController = require('./app/controllers/UserController')
 
-routes.get('/', (req, res) => {
-  return res.render('auth/signup')
-})
+routes.get('/signup', UserController.create)
+routes.post('/signup', UserController.store)
 
 module.exports = routes
