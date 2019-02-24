7e31144f013506c228d9531cb38332c66c1e8923
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2019-02-24 20:29:01 -0300

    Authenticating user

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,26 @@
+const { User } = require('../models')
+
+class SessionController {
+  async create (req, res) {
+    return res.render('auth/signin')
+  }
+
+  async store (req, res) {
+    const { email, password } = req.body
+    const user = await User.findOne({ where: { email } })
+
+    if (!user) {
+      console.log('User not found')
+      return res.redirect('/')
+    }
+
+    if (!(await user.checkPassword(password))) {
+      console.log('Wrong password')
+      return res.redirect('/')
+    }
+
+    return res.redirect('app/dashboard')
+  }
+}
+
+module.exports = new SessionController()
